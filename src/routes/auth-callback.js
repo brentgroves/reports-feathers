99eb59903a15389af08d46ff30884b441bcd67d2
brentgroves <brent.groves@gmail.com>
2022-12-19 19:26:45 -0500

    updated source code

--- src/routes/auth-callback.js
+++ src/routes/auth-callback.js
@@ -9,59 +9,32 @@ const jwt_decode = require('jwt-decode');
 // var querystring = require('querystring');
 var qs = require('qs');
 const logger = require('../logger');
-
+const { authenticate } = require('@feathersjs/express');
+var g_state = 1;
 module.exports = function (app) {
 
   // Initialize our service with any options it requires
   // app.get('/auth', route(req,res));
   // res.render('index.html', { title: 'Express' });
   // https://stackoverflow.com/questions/28352871/in-express-how-do-i-redirect-a-user-to-an-external-url
-  app.get('/auth-callback', async ({ query: { code }}, res,next) => {
-    // https://login.microsoftonline.com/5269b021-533e-4702-b9d9-72acbc852c97/oauth2/v2.0/authorize?client_id=29fa39d4-de57-4009-a46a-c561fa048562&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3030%2Foauth%2Fmicrosoft%2Fcallback&response_mode=query&scope=api%3A%2F%2F29fa39d4-de57-4009-a46a-c561fa048562%2FUser.Info&state=12345
-    // https://docs.feathersjs.com/api/authentication/service.html#to-authenticate-an-external-request
-    // curl -X POST -H 'Content-Type: application/x-www-form-urlencoded' \
-    // https://login.microsoftonline.com/5269b021-533e-4702-b9d9-72acbc852c97/oauth2/v2.0/token \
-    // -d 'client_id=29fa39d4-de57-4009-a46a-c561fa048562' \
-    // -d 'scope=api%3A%2F%2F29fa39d4-de57-4009-a46a-c561fa048562%2FUser.Info' \
-    // -d 'code=0.AXwAIbBpUj5TAke52XKsvIUsl9Q5-ilX3glApGrFYfoEhWK7ALI.AgABAAIAAAD--DLA3VO7QrddgJg7WevrAgDs_wQA9P937T6-z0dpiK7YdVC6F8UOD8DwBimhy-98zS4AouwpxR62p89LkbLmgc20tZ19DqJEbyreORo0ec5hhWI5nwMDYWDbTEojnbrA0VPfYGrdjgXlUN2mc8mqA8ErKcdaMikLWEIjlw4xXl4ApLR5JsF8WT_5YnjvuJaOcpPzewl00W9jkbdl2CswN-A80o8CX4VfbGRk3JR5jr1luWbhepeBXjYSF8QbZoj-WOuLZdQYz3NJJ0DgSzBoovZj-J59San6ApWg3g3SqTW_GJJA7iecPALYJkOwAs_0t6wDwgNyLa7MmImz7Mgs8lR6e3CxImF-_GCzrcBY_qmqxjbvq1F0DjbecLp0CGbOuY1eoSKBjoN48zue2yjrFpPn-KR30eEwIToabtbaPO9t9oMn657I4ZxjhoK03_mKtwLNTEG34TeraE_DB0MEyeVwY9IvHtZJsLoevaFkg9pXl3VUkh7URD7E4_DsTglXUPpscfJPsWfeGJ3H4gybHTi2lw_9w9573k0WVRQPN7bI6hARyyvQEYC8dmriSWA2KTBDI-O5Uv_oIc9cCCAuIyuD0FLhdjrn3eFcVgDGouiPiskD4ujiVBfHwA9IFSSDo6fyaAfY0RBKr_kRTldONgm8T3i_tSZLENHsbHVselh-1l-SZBVuzQavaA2JxjjD9rbEXHTQ2a1wMJ9S&state=12345&session_state=87a1a296-b455-4aaf-b51d-fd0c0150ee05' \
-    // -d 'redirect_uri=http%3A%2F%2Flocalhost%3A3030%2Foauth%2Fmicrosoft%2Fcallback' \
-    // -d 'grant_type=authorization_code' \
-    // -d 'state=12345'    
-    // https://stackoverflow.com/questions/67009128/how-to-get-azure-access-token-with-node-js-api-using-axios
-    // https://stackoverflow.com/questions/31756756/axios-http-client-how-to-construct-http-post-url-with-form-params
-    // https://medium.com/@masnun/handling-timeout-in-axios-479269d83c68
-    // The default timeout is 0 set a longer timeout for the access_token request.
-    // https://blog.bitsrc.io/the-power-of-axios-cf45e085d924
-    // console.log(`before post:${code}`)
-    // console.log(`before post 2`)
-    // console.log('querystring.stringify=>' + querystring.stringify({
-    //   client_id:'29fa39d4-de57-4009-a46a-c561fa048562',
-    //   scope:'api://29fa39d4-de57-4009-a46a-c561fa048562/User.Info',
-    //   code:code,
-    //   redirect_uri:'http://localhost:3030/auth-callback',
-    //   grant_type:'authorization_code',
-    //   state:12345
-    // }))
-    // console.log(`before post 3`)
-    // console.log('qs.stringify=>' + qs.stringify({
-    //   client_id:'29fa39d4-de57-4009-a46a-c561fa048562',
-    //   scope:'api://29fa39d4-de57-4009-a46a-c561fa048562/User.Info',
-    //   code:code,
-    //   redirect_uri:'http://localhost:3030/auth-callback',
-    //   grant_type:'authorization_code',
-    //   state:12345
-    // }))
-    // let myObj = { 
-    //   client_id:'29fa39d4-de57-4009-a46a-c561fa048562',
-    //   scope:'api://29fa39d4-de57-4009-a46a-c561fa048562/User.Info',
-    //   code:code,
-    //   redirect_uri:'http://localhost:3030/auth-callback',
-    //   grant_type:'authorization_code',
-    //   state:12345
-    //  };
-    // logger.info('TEST')
-    // logger.info('This message will include a complete object: %O', myObj);
-    // console.log(`console log`);
+  // https://fusebit.io/blog/oauth-state-parameters-nodejs/?utm_source=www.google.com&utm_medium=referral&utm_campaign=none
+
+  // https://fusebit.io/blog/oauth-state-parameters-nodejs/?utm_source=www.google.com&utm_medium=referral&utm_campaign=none
+  // goal: to get the email from microsoft login to the client so that it can call the authenticate function.
+  // 0. browser script creates a client_id random_number
+  // 1. from browser redirect to /microsoft-login with redirect_url?client_id=client_id 
+  // 2. from app route /auth-callback parse for client_id and code 
+  // 3. use code to get access_token 
+  // 4. on success create message with client_id and email  
+  // 5. browser gets message with random_number then calls authenticate with email. 
+
+  // app.get('/auth-callback', async ({ query: { code }}, res,next) => {
+  app.get('/auth-callback', async (req, res) => {
+      //Extracting code and state
+    const { state, code } = req.query;
+    g_state = state;
+    res.redirect("http://localhost:3000/?session_id=2");
+
     var res2 = axios.post('https://login.microsoftonline.com/5269b021-533e-4702-b9d9-72acbc852c97/oauth2/v2.0/token',
       qs.stringify({
         client_id:'29fa39d4-de57-4009-a46a-c561fa048562',
@@ -69,13 +42,14 @@ module.exports = function (app) {
         code:code,
         redirect_uri:'http://localhost:3030/auth-callback',
         grant_type:'authorization_code',
-        state:12345
+        state:state
       }),{
         // timeout:10, // in ms the default is 0 which is no timeout limit.
         headers: { 
           "Content-Type": "application/x-www-form-urlencoded"
         },
       }).then(function(response) {
+
           // console.log(response.data.access_token);
           const decoded = jwt_decode(response.data.access_token);
           // logger.info('jwt:%s', JSON.stringify(decoded))
@@ -88,15 +62,6 @@ module.exports = function (app) {
           const groups = decoded.groups;
           const password = 'passwordless';
 
-          Can the response contain the email address so the browser can call authenticate? res.status(200).('email')
-          auth responded with a redirect to microsoft's login function.
-          microsoft authenticated the user and did an http request to auth-callback 
-          the browser is waiting for a response from that redirect before allowing the user 
-          to do anything else.
-          If you res.redirect now before creating the user or a jwt then the browser will be
-          able to get the events from these 2 calls. 
-          or use message event to notify browser that we have the jwt. 
-          
           logger.info('jwt: %o', {email,upn,name,family_name,given_name,groups});
           app
           .service('users')
@@ -111,28 +76,13 @@ module.exports = function (app) {
             {
               logger.info('%o', err);
               logger.info('now try to login');
-
-              Don't know if we can call create or authenticate from hear but try and respond by redirecting to / using res.redirect. because 
-              the browser is just waiting until some response is sent back.
-              create(data, params)
-              authService.create(data, params) -> Promise runs authService.authenticate with data, 
-              params and the list of strategies from authStrategies in the configuration. As with any other Feathers service, 
-              this method will be available to clients, e.g. running a POST /authentication.
-
-              If successful it will create a JWT with the payload taken from authService.getPayload and the options from 
-              authService.getTokenOptions. data must always contain a valid and allowed strategy name. Will emit the login event.
-              Try to call authenticate first and if that don't work try to call create().  
-              // app
-              // .service('/authenticate')
-              // .authenticate({
-              //   strategy: 'local',
-              //   email,
-              //   password:'passwordless'
-              // }).catch((err) => { 
-              //   logger.info('%o', err);
-              //   // response.status(500).json({ err: err.message })
-              // });
-
+              // res.redirect("/");
+              app
+              .service('messages')
+              .create({ text:'test',userId:'test' })
+              .catch((err) => { 
+              logger.info('%o', err);
+              });              
               // https://docs.feathersjs.com/api/authentication/service.html#configuration    
               // Call the strategy .authenticate method with data
               // Create a JWT for the entity returned by the strategy
@@ -145,11 +95,12 @@ module.exports = function (app) {
               }).then((authResult) => {
                   logger.info('now try to logout %o',authResult);
                   res.redirect("/");
-                  res.render('index.html', { title: 'Express' });
+                  // res.render('index.html', { title: 'Express' });
               }).catch((err) => { 
                   logger.info('%o', err);
               })
-                  // app.service('/authentication').create(data) 
+              // res.redirect('/');
+              // app.service('/authentication').create(data) 
               // or POST /authentication 
               // with data as { strategy: name, ...loginData }. Internally authentication will then
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,3 +1,4 @@
+// require URLSearchParams;
 function route (req, res) {
   res.redirect(
     `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
@@ -8,16 +9,30 @@ module.exports = function (app) {
 
   // Initialize our service with any options it requires
   // app.get('/auth', route(req,res));
-  app.get('/auth', (req, res) => {
 
-    // app.get('/test', (req, res) => {
+  app.get('/auth', (req, res) => {
+    // const urlEncoded = new URLSearchParams(query).toString();
+  // const { code, client_id } = req.query;
+    const state=11111
+  // app.get('/test', (req, res) => {
     //   res.redirect(
     //     `https://login.microsoftonline.com/5269b021-533e-4702-b9d9-72acbc852c97/oauth2/v2.0/authorize?client_id=29fa39d4-de57-4009-a46a-c561fa048562&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3030%2Foauth%2Fmicrosoft%2Fcallback&response_mode=query&scope=api%3A%2F%2F29fa39d4-de57-4009-a46a-c561fa048562%2FUser.Info&state=12345
     //     `,
     //   );
     // }); 
-    res.redirect(
-      `https://login.microsoftonline.com/5269b021-533e-4702-b9d9-72acbc852c97/oauth2/v2.0/authorize?client_id=29fa39d4-de57-4009-a46a-c561fa048562&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3030%2Fauth-callback&response_mode=query&scope=api%3A%2F%2F29fa39d4-de57-4009-a46a-c561fa048562%2FUser.Info&state=12345`,
+    // res.status(301).redirect("/")
+    // status 301, resource has moved
+    res.status(301).redirect(
+      // test sending client id as state with redirect uri.
+      // http://localhost/auth-callback
+      // http%3A%2F%2Flocalhost%2Fauth-callback%3F11111
+
+      // `https://login.microsoftonline.com/5269b021-533e-4702-b9d9-72acbc852c97/oauth2/v2.0/authorize?client_id=29fa39d4-de57-4009-a46a-c561fa048562&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3030%2Fauth-callback%3Fsession_id%3F11111&response_mode=query&scope=api%3A%2F%2F29fa39d4-de57-4009-a46a-c561fa048562%2FUser.Info&state=12345`,
+      `https://login.microsoftonline.com/5269b021-533e-4702-b9d9-72acbc852c97/oauth2/v2.0/authorize?client_id=29fa39d4-de57-4009-a46a-c561fa048562&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3030%2Fauth-callback&response_mode=query&scope=api%3A%2F%2F29fa39d4-de57-4009-a46a-c561fa048562%2FUser.Info&state=${state}`,
     );
+
+    // res.redirect(
+    //   `https://login.microsoftonline.com/5269b021-533e-4702-b9d9-72acbc852c97/oauth2/v2.0/authorize?client_id=29fa39d4-de57-4009-a46a-c561fa048562&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3030%2Fauth-callback&response_mode=query&scope=api%3A%2F%2F29fa39d4-de57-4009-a46a-c561fa048562%2FUser.Info&state=12345`,
+    // );
   });  
 };
